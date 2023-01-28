4650110873e5848e2b7d3192c8b1909cbce8ff39
ahmed <ahmedyoussofafit@gmail.com>
2023-01-28 11:16:28 +0200

    add project

---
+++ src/main/java/com/example/devops/DevopsDockerApplication.java
@@ -0,0 +1,33 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+@RestController
+@RequestMapping("api")
+public class DevopsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsDockerApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello(){
+		
+		return "hello";
+		
+	}
+	
+	@GetMapping("/welcom")
+	public String weclom(){
+		
+		return "welcom";
+		
+	}
+
+}
